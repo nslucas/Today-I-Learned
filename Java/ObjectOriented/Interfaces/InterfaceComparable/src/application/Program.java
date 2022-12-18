1f72a317f8358d32dd37b195d28582a2799348dc
nslucas <lucasn.nunes066@gmail.com>
2022-12-18 18:54:18 -0300

    Learned how to use the Interface 'Comparable' and how to sort it in different ways

---
+++ Java/ObjectOriented/Interfaces/InterfaceComparable/src/application/Program.java
@@ -0,0 +1,33 @@
+package application;
+
+import entities.Employee;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Program {
+    public static void main(String[] args) {
+        List<Employee> list = new ArrayList<>();
+        String path = "C:\\temp\\emp.txt";
+
+        try(BufferedReader br = new BufferedReader(new FileReader(path))){
+            String employeeCsv = br.readLine();
+
+            while(employeeCsv != null){
+                String[] aux = employeeCsv.split(",");
+                list.add(new Employee(aux[0], Double.parseDouble(aux[1])));
+                employeeCsv = br.readLine();
+            }
+            Collections.sort(list);
+            for(Employee emp : list){
+                System.out.println(emp.getName() + ", " + emp.getSalary());
+            }
+        }catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
