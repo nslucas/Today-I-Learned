6ee9f28855c29d49fc16c5c25f7f967c27de808d
nslucas <lucasn.nunes066@gmail.com>
2022-12-21 19:40:09 -0300

    learning to use Hash and Equals

---
+++ Java/Generics - Set - Map/HashCode/src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import entities.Client;
+
+public class Program {
+    public static void main(String[] args) {
+        Client c1 = new Client("Maria", "maria@gmail.com");
+        Client c2 = new Client("Maria", "maria@gmail.com");
+
+        System.out.println(c1.hashCode());
+        System.out.println(c2.hashCode());
+        System.out.println(c1.equals(c2));
+        System.out.println(c1 == c2);
+    }
+}
