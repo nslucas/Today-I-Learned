6ada86d6d6d8c7821728b681f49278372284c866
Lucas Nunes <lucasn.nunes066@gmail.com>
2022-12-28 13:37:43 -0300

    using HashSet

---
+++ Java/Generics - Set - Map/Set-Hash- Tree/Set-HashSet-TreeSet/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+import java.util.Set;
+public class Main {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        set.add("Tv");
+        set.add("Notebook");
+        set.add("Tablet");
+        System.out.println(set.contains("Notebook"));
+        for (String p : set) {
+            System.out.println(p);
+        }
+    }
+}
