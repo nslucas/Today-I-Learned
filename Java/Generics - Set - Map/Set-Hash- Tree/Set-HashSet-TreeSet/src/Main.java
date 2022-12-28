b403c25e56510aaec78406773922e8b89158bbbb
Lucas Nunes <lucasn.nunes066@gmail.com>
2022-12-28 13:40:13 -0300

    using LinkedHashSet and maintaning the order of insertion

--- Java/Generics - Set - Map/Set-Hash- Tree/Set-HashSet-TreeSet/src/Main.java
+++ Java/Generics - Set - Map/Set-Hash- Tree/Set-HashSet-TreeSet/src/Main.java
@@ -1,10 +1,11 @@
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
-        Set<String> set = new TreeSet<>();
+        Set<String> set = new LinkedHashSet<>();
         set.add("Tv");
         set.add("Notebook");
         set.add("Tablet");
