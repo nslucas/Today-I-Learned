d2e66627a5f03d26c6a52c5dad26b49bec0eb256
Lucas Nunes <lucasn.nunes066@gmail.com>
2022-12-28 14:36:17 -0300

    working with numerical sets

---
+++ Java/Generics - Set - Map/Set-Hash- Tree/Set-HashSet-TreeSet/src/Program.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+public class Program {
+    public static void main(String[] args) {
+        Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10));
+        Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
+//union
+        Set<Integer> c = new TreeSet<>(a);
+        c.addAll(b);
+        System.out.println("Union " + c);
+//intersection
+        Set<Integer> d = new TreeSet<>(a);
+        d.retainAll(b);
+        System.out.println("Intersection " + d);
+//difference
+        Set<Integer> e = new TreeSet<>(a);
+        e.removeAll(b);
+        System.out.println("Difference " + e);
+    }
+}
