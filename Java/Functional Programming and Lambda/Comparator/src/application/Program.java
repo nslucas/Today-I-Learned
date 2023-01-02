dc0f11a3c2fbe32d1dca8a75aab57b715907d8c2
Lucas Nunes <lucasn.nunes066@gmail.com>
2023-01-01 22:47:12 -0300

    learning how to use Lambda expressions, replacing the interface Comparator in this example

---
+++ Java/Functional Programming and Lambda/Comparator/src/application/Program.java
@@ -0,0 +1,22 @@
+package application;
+import java.util.*;
+
+import entities.MyComparator;
+import entities.Product;
+
+public class Program {
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 450.00));
+
+        list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+
+        for (Product p: list) {
+            System.out.println(p);
+        }
+    }
+}
