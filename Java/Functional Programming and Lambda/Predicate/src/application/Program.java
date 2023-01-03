ed89d63a5efc50f32a2d2716b4364ec59aec07be
Lucas Nunes <lucasn.nunes066@gmail.com>
2023-01-03 17:23:30 -0300

    lambda using the Predicate interface

---
+++ Java/Functional Programming and Lambda/Predicate/src/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+
+import entities.Product;
+import util.ProductPredicate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        list.removeIf(new ProductPredicate());
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+
+    }
+}
