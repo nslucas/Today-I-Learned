0eb9ae431ae348e303d0737d3d15525d20220cda
Lucas Nunes <lucasn.nunes066@gmail.com>
2023-01-04 19:06:44 -0300

    reference method with a static method with the same logic as the consumer interface

--- Java/Functional Programming and Lambda/Consumer/src/entities/Product.java
+++ Java/Functional Programming and Lambda/Consumer/src/entities/Product.java
@@ -26,6 +26,10 @@ public class Product {
         this.price = price;
     }
 
+    public static void staticPriceUpdate(Product p){
+        p.setPrice(p.getPrice() * 1.1);
+    }
+
     @Override
     public String toString() {
         return name + ", " + String.format("%.2f", price);
