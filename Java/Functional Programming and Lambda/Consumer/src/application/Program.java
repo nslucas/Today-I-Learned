82a7208cc2734b868547a2df44a0ac7ff01a3d6e
Lucas Nunes <lucasn.nunes066@gmail.com>
2023-01-04 19:14:14 -0300

    declaring lambda function with consumer

--- Java/Functional Programming and Lambda/Consumer/src/application/Program.java
+++ Java/Functional Programming and Lambda/Consumer/src/application/Program.java
@@ -6,6 +6,8 @@ import util.PriceUpdate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
+import java.util.stream.LongStream;
 
 public class Program {
     public static void main(String[] args) {
@@ -18,8 +20,10 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        list.forEach(Product::staticPriceUpdate);
+        double factor = 1.1;
+        Consumer<Product> cons = p -> p.setPrice(p.getPrice() * factor);
 
+        list.forEach(cons);
         list.forEach(System.out::println);
     }
 }
--- Java/Functional Programming and Lambda/Consumer/src/entities/Product.java
+++ Java/Functional Programming and Lambda/Consumer/src/entities/Product.java
@@ -26,10 +26,6 @@ public class Product {
         this.price = price;
     }
 
-    public static void staticPriceUpdate(Product p){
-        p.setPrice(p.getPrice() * 1.1);
-    }
-
     @Override
     public String toString() {
         return name + ", " + String.format("%.2f", price);
