f573665c031c67e3ac7612730c76d2991d5c057e
Lucas Nunes <lucasn.nunes066@gmail.com>
2023-01-04 19:04:27 -0300

    implementing the Consumer<T> interface

---
+++ Java/Functional Programming and Lambda/Consumer/src/application/Program.java
@@ -0,0 +1,25 @@
+package application;
+
+import entities.Product;
+import util.PriceUpdate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        list.forEach(new PriceUpdate());
+
+        list.forEach(System.out::println);
+    }
+}
