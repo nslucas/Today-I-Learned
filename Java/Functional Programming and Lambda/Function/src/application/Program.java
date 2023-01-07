65c1830d004fbe4043f36f35c772aca12d62a857
Lucas Nunes <lucasn.nunes066@gmail.com>
2023-01-07 17:09:28 -0300

    implementing the Function interface

---
+++ Java/Functional Programming and Lambda/Function/src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import entities.Product;
+import util.UpperCaseName;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+
+        names.forEach(System.out::println);
+
+    }
+}
