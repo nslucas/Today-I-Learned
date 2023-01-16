1eb5fe38c94fe812e9a83bfb1e4c479bed73f332
Lucas Nunes <lucasn.nunes066@gmail.com>
2023-01-16 17:07:53 -0300

    getting info from the database

--- Java/JavaFX/javafx/src/application/Program.java
+++ Java/JavaFX/javafx/src/application/Program.java
@@ -1,15 +1,40 @@
 package application;
 
 import db.DB;
+import db.DbException;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
 
     public static void main(String[] args){
 
-        Connection conn = DB.getConnection();
-        DB.closeConnection();
+        Connection conn = null;
+
+        Statement st = null;
+
+        ResultSet rs = null;
+
+        try {
+            conn = DB.getConnection();
+
+            st = conn.createStatement();
+
+            rs = st.executeQuery("SELECT * FROM department");
+
+            while(rs.next()) {
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
 
     }
 }
