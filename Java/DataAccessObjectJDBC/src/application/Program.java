cd052159b3c86b41588360b31812aba60226b266
Lucas Nunes <lucasn.nunes066@gmail.com>
2023-01-21 14:31:05 -0300

    insert implementation

--- Java/DataAccessObjectJDBC/src/application/Program.java
+++ Java/DataAccessObjectJDBC/src/application/Program.java
@@ -33,5 +33,10 @@ public class Program {
             System.out.println(obj);
         }
 
+        System.out.println("\n=== TESTE 4: seller insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+
+        System.out.println("Inserted! New Id = " + newSeller.getId());
     }
 }
