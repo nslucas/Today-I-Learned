5c1595cabdd1d44d772d8227ebb338946cbe17d7
Lucas Nunes <lucasn.nunes066@gmail.com>
2023-01-22 17:10:03 -0300

    test commit

--- Java/SpringBoot/course/src/main/java/com/educandoweb/course/CourseApplication.java
+++ Java/SpringBoot/course/src/main/java/com/educandoweb/course/CourseApplication.java
@@ -9,7 +9,7 @@ public class CourseApplication {
     public static void main(String[] args) {
         SpringApplication.run(CourseApplication.class, args);
 
-        System.out.println("hello world!");
+        System.out.println("hello world! teste");
     }
 
 }
