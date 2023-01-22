6d164e10041ebb494559d709fe662650befc6770
Lucas Nunes <lucasn.nunes066@gmail.com>
2023-01-22 17:39:21 -0300

    H2 database, test profile, JPA

--- Java/SpringBoot/course/src/main/java/com/educandoweb/course/entities/User.java
+++ Java/SpringBoot/course/src/main/java/com/educandoweb/course/entities/User.java
@@ -1,10 +1,17 @@
 package com.educandoweb.course.entities;
 
+import jakarta.persistence.*;
+import jdk.jfr.Enabled;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
@@ -73,4 +80,5 @@ public class User implements Serializable {
     public int hashCode() {
         return Objects.hash(id);
     }
+
 }
