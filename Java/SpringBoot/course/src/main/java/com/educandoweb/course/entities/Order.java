1629cd2bf27a3e2f91bf66d452f9f4f436594179
Lucas Nunes <lucasn.nunes066@gmail.com>
2023-01-23 21:24:18 -0300

    Order, instant, ISO 8601 - final implementation

--- Java/SpringBoot/course/src/main/java/com/educandoweb/course/entities/Order.java
+++ Java/SpringBoot/course/src/main/java/com/educandoweb/course/entities/Order.java
@@ -1,4 +1,5 @@
 package com.educandoweb.course.entities;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -12,6 +13,7 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
     @ManyToOne
--- Java/SpringBoot/course/src/main/java/com/educandoweb/course/entities/User.java
+++ Java/SpringBoot/course/src/main/java/com/educandoweb/course/entities/User.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -19,6 +20,7 @@ public class User implements Serializable {
     private String password;
 
     @OneToMany(mappedBy = "client")
+    @JsonIgnore
     private List<Order> orders = new ArrayList<>();
 
     public User(){}
