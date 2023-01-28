e8ac09a84367649bcb2957562aa89ce3bf57fe32
Lucas Nunes <lucasn.nunes066@gmail.com>
2023-01-28 14:52:42 -0300

    Product

---
+++ Java/SpringBoot/course/src/main/java/com/educandoweb/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.educandoweb.course.repositories;
+
+import com.educandoweb.course.entities.Category;
+import com.educandoweb.course.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
