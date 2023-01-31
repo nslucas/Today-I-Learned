a3825b3a6247e9272071134e626fe440cd8a6eb2
Lucas Nunes <lucasn.nunes066@gmail.com>
2023-01-31 18:14:29 -0300

    subtotal & total methods

--- Java/SpringBoot/course/src/main/java/com/educandoweb/course/config/TestConfig.java
+++ Java/SpringBoot/course/src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -64,8 +64,8 @@ public class TestConfig implements CommandLineRunner {
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 
         OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
-        OrderItem oi2 = new OrderItem(o1, p3, 2, p4.getPrice());
-        OrderItem oi3 = new OrderItem(o2, p3, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p4.getPrice());
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
