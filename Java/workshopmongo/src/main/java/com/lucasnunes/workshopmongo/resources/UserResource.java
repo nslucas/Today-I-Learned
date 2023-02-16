4e482ba42b4644fefcbe81e56ba1fcc3b3bdf97d
Lucas Nunes <lucasn.nunes066@gmail.com>
2023-02-16 19:05:12 -0300

    DTO pattern to get users

--- Java/workshopmongo/src/main/java/com/lucasnunes/workshopmongo/resources/UserResource.java
+++ Java/workshopmongo/src/main/java/com/lucasnunes/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.lucasnunes.workshopmongo.resources;
 
 import com.lucasnunes.workshopmongo.domain.User;
+import com.lucasnunes.workshopmongo.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.lucasnunes.workshopmongo.services.UserService;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -18,8 +20,9 @@ public class UserResource {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
